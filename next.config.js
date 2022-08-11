e6450d64c468f591c198807d610ae933360c17e5
DenisMPar <paradadenismartin@gmail.com>
2022-08-10 23:40:56 -0300

    icons components added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const withSvgr = require("next-svgr");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = withSvgr({
+  // your config for other plugins or the general next.js here...
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  compiler: {
+    styledComponents: true,
+  },
+});
