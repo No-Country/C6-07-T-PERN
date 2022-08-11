a22f0f13147090a03d07478ec5203e5ada1a20bf
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-08-10 23:41:57 -0300

    added button and styles

--- ui/buttons/index.js
+++ ui/buttons/index.js
@@ -1,4 +1,4 @@
 import css from "./index.module.css";
 export function PrimaryButton() {
-  return <button className={css.blue}>Hola</button>;
+  return <button className={css.bgcolor}>Hola</button>;
 }
