ef849a15a680d76b46d82439bd2313b9aca9cb79
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-08-13 21:28:58 -0300

    updated Footer component and Styles

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,17 +1,20 @@
-import { DarkLogo, InstagramIcon, TwitterIcon } from "../../ui/icons/index.js";
+import { DarkLogo, FacebookIcon, InstagramIcon, TwitterIcon } from "../../ui/icons/index.js";
 import css from "./index.module.css";
 
 export default function Footer() {
   return (
     <div className={css.divFooterMain}>
-      <div className={css.divFooterLogo}>
+		<div className={css.divFooterLogo}>
         <DarkLogo />
-      </div>
-      <div className={css.divFooterIcons}>
-        <InstagramIcon />
+        </div>
+        <div className={css.divFooterIcons}>
+		<FacebookIcon />
         <TwitterIcon />
-      </div>
-      <div className={css.copyrightFooter}>Todos los derechos reservados</div>
+		<InstagramIcon />
+        </div>
+		<div className={css.copyrightFooter}> 
+		<div>Todos los derechos reservados</div>   
+		</div>
     </div>
   );
 }
