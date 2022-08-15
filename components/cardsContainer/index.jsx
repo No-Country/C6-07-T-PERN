604626b946174e94ad43070b86762e4a4d031e54
Nano <fgomeztneorio@hotmail.com>
2022-08-14 20:09:51 -0500

    Cards logic done

---
+++ components/cardsContainer/index.jsx
@@ -0,0 +1,45 @@
+//Nano: Importado de librerias:
+import { connect, useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+//Nano: Importado de archivos propios:
+import { getMedia } from "../../store/actions";
+import Card from "../card";
+import css from "./index.module.css";
+
+//Nano: Mapeo de los estados de redux con las props del elemento
+function mapStateToProps(state) {
+  return {
+    media: state.media,
+  };
+}
+
+//Nano: Mapeo de los funciones dispatch de redux con las props del elemento
+function mapDispatchToProps(dispatch) {
+  return {
+    getMedia: () => dispatch(getMedia()),
+  };
+}
+
+//Nano: Función principal para contrucción de la etiqueta
+function CardsContainer() {
+  //Nano: Asociación del objeto media y los dispátch con las props:
+  const media = useSelector((state) => state.media);
+  const dispatch = useDispatch();
+  //Nano: Aministración de estados de la etiqueta Card
+  useEffect(() => {
+    dispatch(getMedia());
+  }, []);
+
+  //Nano: Devolución de la etiqueta
+  return (
+    <div className="divCardContainer">
+      {media.map((element) => {
+        return <Card key={element.id} media={element} />;
+      })}
+    </div>
+  );
+}
+
+//Nano: Exportación por defecto de la etiqueta para llamos externos
+export default connect(mapStateToProps, mapDispatchToProps)(CardsContainer);
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,20 +1,25 @@
-import { DarkLogo, FacebookIcon, InstagramIcon, TwitterIcon } from "../../ui/icons/index.js";
+import {
+  DarkLogo,
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+} from "../../ui/icons/index.js";
 import css from "./index.module.css";
 
 export default function Footer() {
   return (
     <div className={css.divFooterMain}>
-		<div className={css.divFooterLogo}>
+      <div className={css.divFooterLogo}>
         <DarkLogo />
-        </div>
-        <div className={css.divFooterIcons}>
-		<FacebookIcon />
+      </div>
+      <div className={css.divFooterIcons}>
+        <FacebookIcon />
         <TwitterIcon />
-		<InstagramIcon />
-        </div>
-		<div className={css.copyrightFooter}> 
-		<div>Todos los derechos reservados</div>   
-		</div>
+        <InstagramIcon />
+      </div>
+      <div className={css.copyrightFooter}>
+        <div>Todos los derechos reservados</div>
+      </div>
     </div>
   );
 }
