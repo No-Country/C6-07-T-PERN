0e15fecce4f74703b7ec5ab01822b7de4a7d6465
DenisMPar <paradadenismartin@gmail.com>
2022-08-18 22:06:45 -0300

    added new colors to base.css, added ui/text components

--- ui/text/index.js
+++ ui/text/index.js
@@ -1,16 +1,27 @@
 import css from "./index.module.css";
 
 const classH2 = css.title + " " + css.text;
-console.log(classH2);
+const classH3 = css.subtitle + " " + css.text;
+const classH3semiBold = css.subtitleSemiBold + " " + css.text;
+const classH4 = css.body + " " + css.text;
+const classH4Buttons = css.buttons + " " + css.text;
+const classH5 = css.tiny + " " + css.text;
+
 export function H2(props) {
-  return <h1 className={classH2}>{props.children}</h1>;
+  return <h2 className={classH2}>{props.children}</h2>;
 }
-export function H3() {
-  return <h2 className={css.text}>title</h2>;
+export function H3(props) {
+  return <h3 className={classH3}>{props.children}</h3>;
 }
-export function H4() {
-  return <h3 className={css.text}>title</h3>;
+export function H3semiBold(props) {
+  return <h3 className={classH3semiBold}>{props.children}</h3>;
 }
-export function H5() {
-  return <h4 className={css.text}>title</h4>;
+export function H4(props) {
+  return <p className={classH4}>{props.children}</p>;
+}
+export function H4Buttons(props) {
+  return <p className={classH4Buttons}>{props.children}</p>;
+}
+export function H5(props) {
+  return <p className={classH5}>{props.children}</p>;
 }
