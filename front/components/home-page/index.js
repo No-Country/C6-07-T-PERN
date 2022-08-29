ace6c95ec2de66d8ce8423bc347c2f2e606ba040
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-08-29 13:26:42 -0300

    added SignUp function to Homepage

--- front/components/home-page/index.js
+++ front/components/home-page/index.js
@@ -2,13 +2,14 @@ import { H2 } from "../../ui/text";
 import CardsContainer from "../cardsContainer";
 import PlatformFilters from "../platform-filters";
 import FilterContainer from "../filterContainer";
-import Login from "../login";
+import {Login, SignUp} from "../login";
 import css from "./index.module.css";
 export default function HomePage() {
   return (
     <div className={css.homePage}>
       <PlatformFilters></PlatformFilters>
       <Login />
+	  <SignUp />
       <FilterContainer />
       <H2>Populares</H2>
       <CardsContainer></CardsContainer>
