25cfdd170564782fc4d913631807340d5390733b
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-09-01 18:04:07 -0300

    updated the profilePage component

--- front/components/profile-page/index.js
+++ front/components/profile-page/index.js
@@ -1,18 +1,26 @@
 import { useRouter } from "next/router";
 import { logout } from "../../lib";
 import { BorderlessButton } from "../../ui/buttons";
-import { H2 } from "../../ui/text";
+import { H2, H3, H5 } from "../../ui/text";
 
-export default function ProfilePage() {
+export default function ProfilePage(props) {
   const router = useRouter();
   async function handleClick() {
     await logout();
     router.push("/");
   }
   return (
-    <div style={{ backgroundColor: "black", height: "80vh" }}>
-      <H2>Perfil</H2>
-      <BorderlessButton onClick={handleClick}>Cerrar sesión</BorderlessButton>
+    <div style={{ backgroundColor: "black", height: "80vh", margin: "12px" }}>
+      <H2>Mi perfil</H2>
+      <div style={{ padding:"2px" }}>
+        <H3>{props.username}Alicia Ramirez</H3>
+        <H5>{props.email}aliciaramirez@gmail.com</H5>
+      </div>
+      <div style={{ padding: "2px" }}>
+        <H3>País</H3>
+        <H3>{props.country}Argentina</H3>
+      </div>
+      <div style={{ display: "flex", flexDirection: "row-reverse" }}><BorderlessButton onClick={handleClick}>Cerrar sesión</BorderlessButton></div>
     </div>
   );
 }
